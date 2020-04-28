81d2d8d3d79160daf3fa2b6b4669e6d827794dad
njager <nicholasejager@gmail.com>
2020-04-28 00:45:29 -0400

    Updated scripts
    Filled out most of enemy script, completed coin script, added some methods and stats to player script, added Actor script to base physics off of

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,2 +1,41 @@
-extends KinematicBody2D
+extends Actor
+
+#stats
+var curHp : int = 3
+var maxHp : int = 3
+var damage : int = 1
+var atttackRate : float = 1.0
+var moveSpeed : float = 4
+
+export var damageToTake : int = 1
+
+
+#components
+onready var timer = get_node("Timer")
+onready var player = get_node("res://Scenes/Player.tscn")
+onready var attack_is_ready : bool
+
+func _ready():
+	timer.wait_time = atttackRate
+	attack_is_ready = false
+	_velocity.x = -speed.x
+
+func _physics_process(delta):
+	_velocity.x *= -1 if is_on_wall() else 1
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+func _on_Area2D_body_entered(body):
+	if attack_is_ready == true:
+		player.take_damage()
+
+func _on_Timer_timeout():
+	attack_is_ready = true
+
+func take_damage(damageToTake):
+	curHp -= damageToTake
+	if curHp <= 0:
+		die()
+
+func die():
+	queue_free()
 
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,2 +1,23 @@
-extends KinematicBody2D
+extends Actor
 
+#stats
+var score : int = 0
+var curHp : int = 10
+var maxHp : int = 10
+var damage : int = 1
+
+#components
+onready var ui = get_node("CanvasLayer/UI")
+
+func give_score(amount):
+	score += amount
+	ui.update_score_text(score)
+
+func take_damage(damageToTake):
+	curHp -= damageToTake
+	ui.update_health_bar(curHp, maxHp)
+	if curHp <= 0:
+		die()
+
+func die():
+	pass
